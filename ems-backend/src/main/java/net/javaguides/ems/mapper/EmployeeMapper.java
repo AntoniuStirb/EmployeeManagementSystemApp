9ed23af045937dd8963690d0ca57f89fc23272cd
Antoniu Stirb <antoniu_stirb@yahoo.com>
2024-02-29 12:49:23 +0200

    commit

---
+++ ems-backend/src/main/java/net/javaguides/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package net.javaguides.ems.mapper;
+
+import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){ //convert from employee to employeeDTO
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){ //convert from employeeDTO to employee
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
